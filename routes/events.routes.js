272cf76c6c309a024d4a2a480927b421f9fb4112
John Armbruster <jaboyak@gmail.com>
2017-01-25 12:56:36 -0700

    updated populate and updated add/update for events routes

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var populate = require('../lib/populate');
+
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
@@ -9,41 +10,28 @@ router.use(function timeLog(req, res, next) {
 });
 // get all events
 router.get('/all', function (req, res) {
-    // knex.raw('select * from "events"')
-    //     .then(function(events) {
-    //       // res.send(events.rows)
-    //       populate(events.rows);
-    //       res.send('blah')
-    //     })
-    // populate('events', 'performers', 'id').then(function(response) {
-    //   console.log('response: ', response)
-    //   res.send(response);
-    // });
-    Promise.all([
-        knex.raw('select * from events'),
-        knex.raw('select * from performers')
-    ]).then(function (result) {
-        var populatedArr = populate(result, 'events', 'performer_id', 'performers', 'id');
+    populate.getAll('events', 'performers').then(function (result) {
+        var populatedArr = populate.mapAllToObj(result, 'performer_id', 'performers');
         res.send(populatedArr);
     });
 });
 // get single event
 router.get('/:id', function (req, res) {
-    knex.raw('select * from "events" where id = ?', req.params.id)
-        .then(function (event) {
-        res.send(event);
-    });
+  populate.getOne({table: 'events', id: req.params.id}, 'performers').then(function (result) {
+      var populatedArr = populate.mapSingleToObj(result, 'performer_id', 'performers');
+      res.send(populatedArr);
+  });
 });
 // create single event
-router.get('/add', function (req, res) {
-    knex.raw('select * from "events" where id = ?', req.params.id)
+router.post('/add', function (req, res) {
+    knex.raw('insert into events(name, performer_id, location_city, location_state) values(?, ?, ?, ?)', [req.body.name, req.body.performer_id, req.body.location_city, req.body.location_state])
         .then(function (event) {
         res.send(event);
     });
 });
 // edit/update single event
 router.put('/:id', function (req, res) {
-    knex.raw('select * from "events" where id = ?', req.params.id)
+    knex.raw("update events set "+ req.body.field +" = ? where id = ?", [req.body.value, req.params.id])
         .then(function (event) {
         res.send(event);
     });
